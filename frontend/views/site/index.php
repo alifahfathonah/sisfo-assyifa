d3f86c3c58e910dd7301843a5bfe0c7f09beba1c
hamzahfauzy <hamzahfauzy97@gmail.com>
2020-10-13 18:58:28 +0700

    fix front end

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -27,7 +27,11 @@ $this->title = 'Sistem Informasi STIKES As-Syifa';
                     <h4><?=$app->nama?></h4>
                     <img src="<?=Yii::$app->params['backend_url']?>/uploads/<?=$app->icon?>" alt="<?=$app->nama?>" width="100px">
                     <br><br>
+                    <?php if(in_array($app->nama,['Perpustakaan','OJS'])): ?>
+                    <p><a class="btn btn-default" href="<?=$app->url?>">Ke Aplikasi &raquo;</a></p>
+                    <?php else: ?>
                     <p><a class="btn btn-default" href="<?=$app->url?>/site/login-by-token?token=<?=Yii::$app->user->identity->auth_key?>">Login Ke Aplikasi &raquo;</a></p>
+                    <?php endif ?>
                 </center>
             </div>
             <?php endforeach ?>
